0f6003628420e5a64768384d1eed7aee6e473c55
kenckl <klcheung8355@gmail.com>
2020-11-26 14:13:26 +0800

    [fizz-buzz][ken]refactor code FizzBuzz.java

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,15 +2,22 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
+    private static final int MODULO_3 = 3;
+    private static final int MODULO_5 = 5;
+    private static final int MODULO_7 = 7;
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String WHIZZ = "Whizz";
+
     public String countOff(int orderNum){
-        if (orderNum % 3 == 0){
-            return "Fizz";
+        if (orderNum % MODULO_3 == 0){
+            return FIZZ;
         }
-        if (orderNum % 5 == 0){
-            return "Buzz";
+        if (orderNum % MODULO_5 == 0){
+            return BUZZ;
         }
-        if (orderNum % 7 == 0){
-            return "Whizz";
+        if (orderNum % MODULO_7 == 0){
+            return WHIZZ;
         }
 
         return String.valueOf(orderNum);
